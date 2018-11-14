ba428aabda30dd01ddfc9d55d82717a458191d85
ben grosser <bgrosser0@mncallysmith.edu>
2018-11-14 15:25:48 -0600

    intial server set up

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://ben:ben123@ds061370.mlab.com:61370/mern_shopping"
+}
--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+const app = express();
+
+// BODY PARSER MIDDLEWARE
+app.use(bodyParser.json())
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// CONNECT TO MONGO
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server started on port 5000'));
+// START SERVER USING npm run server
